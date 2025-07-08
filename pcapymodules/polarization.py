40f83489eeefceb9fb7cfff52aafb2e29b7925b5
Pratap Chandra Adak <pratapadak.phys@gmail.com>
2025-07-08 15:47:57 -0400

    Clean up

--- pcapymodules/polarization.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def degree_of_polarization(array, angles):
-    minv = min(array)
-    maxv=max(array)
-    min_i = np.argmin(array)
-    max_i = np.argmax(array)
-    return ((maxv-minv)/(minv+maxv), angles[min_i],angles[max_i])
