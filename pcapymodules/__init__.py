e7330511381f7645f44f5be4e89c888a00781ab5
Pratap Chandra Adak <pratapadak.phys@gmail.com>
2025-07-23 12:50:26 -0400

    Update __init__.py

--- pcapymodules/__init__.py
+++ pcapymodules/__init__.py
@@ -8,6 +8,6 @@ Created on Tue Dec 20 12:33:37 2022
 from .analysis.analysis import *
 from .analysis.loadfile import *
 from .analysis.microwave import *
-from .instruments.carbide import *
-from .instruments.lightfield import *
+#from .instruments.carbide import *
+#from .instruments.lightfield import *
 from .instruments.pna import *
